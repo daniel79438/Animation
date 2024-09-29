dfe832a6b2253287e9436dd19f822abb426e3f40
Daniel Harris <daniel79438@gmail.com>
2024-09-29 21:14:30 +0100

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Daniel Harris on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
